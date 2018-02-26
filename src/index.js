401836fb22828886d6f15024c036dae269642c9f
Justin Sitter <justin@jaytria.com>
2018-02-26 13:21:56 -0800

    track moves

--- src/index.js
+++ src/index.js
@@ -78,6 +78,17 @@ function Square(props){
         const current = history[history.length - 1];
         const winner = calculateWinner(current.squares);
 
+        const moves = history.map((step, move)=>{
+            const desc = move ?
+                'Go to move #' + move :
+                'Go to game start';
+            return (
+                <li>
+                    <button onClick={()=> this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
+
         let status;
         if(winner){
             status = 'Winner: ' + (this.state.xIsNext ? 'X' : 'O');
@@ -85,16 +96,16 @@ function Square(props){
 
         return (
             <div className="game">
-            <div className="game-board">
-                <Board 
-                    squares={current.squares}
-                    onClick={(i) => this.handleClick(i)}
-                />
-            </div>
-            <div className="game-info">
-                <div>{status}</div>
-                <ol>{/* TODO */}</ol>
-            </div>
+                <div className="game-board">
+                    <Board 
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}
+                    />
+                </div>
+                <div className="game-info">
+                    <div>{status}</div>
+                    <ol>{moves}</ol>
+                </div>
             </div>
         );
     }
